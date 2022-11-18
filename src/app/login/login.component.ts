6fc4b14130aad4883cf228bcc8eb020868b42df7
LeperlierO <leperlierolivier3@gmail.com>
2022-11-18 10:51:20 +0100

    check form login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { toast } from 'bulma-toast';
 import { BodyLogin, Token } from '../models/auth';
@@ -12,19 +13,34 @@ import { AuthService } from '../services/auth.service';
 export class LoginComponent implements OnInit {
 
   localStorageKey = 'ifp-user';
-  body: BodyLogin = new BodyLogin();
-  login = '';
-  password = '';
+  
+  // Permet de gérer les erreurs de champs obligatoires
+  loginForm: FormGroup | any;
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required, Validators.minLength(6)]);
 
-  constructor(private authService: AuthService,  public router: Router) { }
+  constructor(private authService: AuthService,  public router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     if(this.authService.tokenValue) this.router.navigate(['/']);
+    this.initForm();
     window.scroll(999,999);
   }
 
-  tryLogin(){
-    this.authService.login(this.body).subscribe(
+  initForm(){
+    this.loginForm = this.formBuilder.group({
+      email: this.email,
+      password : this.password,
+    })
+  }
+
+  login(){
+    const formValue = this.loginForm?.value;
+    const body = new BodyLogin();
+    body.email = formValue['email'];
+    body.password = formValue['password'];
+
+    this.authService.login(body).subscribe(
       (token: Token | null) => {
         localStorage.setItem(this.localStorageKey, JSON.stringify(token));
         this.router.navigate(["/"]);
