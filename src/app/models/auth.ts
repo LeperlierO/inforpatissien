883674e4def8beb9235fe1c91f5af5d6e1e788de
LeperlierO <leperlierolivier3@gmail.com>
2022-12-15 11:07:09 +0100

    Game v 1.1.1

--- src/app/models/auth.ts
+++ src/app/models/auth.ts
@@ -27,3 +27,10 @@ export class User{
     gamer!: Boolean;
     avatarUrl!: string;
 }
+
+export const dad = new User('Dominique', 'elpadre', true, 'https://inforpatissien-api.azurewebsites.net/assets/images/users/dad.png');
+export const mum = new User('Pascale', 'elmadre', true, 'https://inforpatissien-api.azurewebsites.net/assets/images/users/mum.png');
+export const sis = new User('Eva', 'elfashion', true, 'https://inforpatissien-api.azurewebsites.net/assets/images/users/sis.png');
+export const bro = new User('Olivier', 'eljunior', true, 'https://inforpatissien-api.azurewebsites.net/assets/images/users/bro.png');
+export const bil = new User('Ancelin', 'elgolfista', true, 'https://inforpatissien-api.azurewebsites.net/assets/images/users/bil.png');
+export const beb = new User('Linda', 'elbacalao', true, 'https://inforpatissien-api.azurewebsites.net/assets/images/users/beb.png');
--- src/app/models/comment.ts
+++ src/app/models/comment.ts
@@ -1,4 +1,4 @@
-import { User } from "./auth";
+import { dad, mum, sis, bro, bil, beb, User } from "./auth";
 
 export class Comment
 {
@@ -13,30 +13,28 @@ export class Comment
     minutes!: number
 }
 
-export const broComments = [
-    new Comment("Mais c'est trop beau mon petit !!", 17, 
-                    new User('Dominique', 'elpadre', true, 'https://inforpatissien-api.azurewebsites.net/assets/images/users/dad.png')),
-    new Comment("Waaaaw junior !!", 32, 
-                    new User('Eva', 'fashionweek', true, 'https://inforpatissien-api.azurewebsites.net/assets/images/users/sis.png')),
-];
+export const broComments = [];
 
 export const sisComments = [
-    new Comment("Mais c'est trop beau ma fille !!", 17, 
-                    new User('Dominique', 'elpadre', true, 'https://inforpatissien-api.azurewebsites.net/assets/images/users/dad.png')),
-    new Comment("Waaaaw Fashion Week !!", 32, 
-                    new User('Eva', 'fashionweek', true, 'https://inforpatissien-api.azurewebsites.net/assets/images/users/sis.png')),
+    new Comment("Mais waaaaaaaaaaaaaaaw ! 🎄", 16, beb),
+    new Comment("Bravo pitchounette ton gâteau est digne d'un repas de fêtes !", 58, mum),
+    new Comment("Oté le gâteau lé vraiment bo là!", 17, dad),
+    new Comment("... 🤮", 17, bil),
+    new Comment("J'ai du soucis à me faire mouais, comment je vais faire pour rester la pâtissier de la famille ...", 39, bro),
 ];
 
 export const dadComments = [
-    new Comment("Mais c'est trop beau moi !!", 17, 
-                    new User('Dominique', 'elpadre', true, 'https://inforpatissien-api.azurewebsites.net/assets/images/users/dad.png')),
-    new Comment("Waaaaw Papa !!", 32, 
-                    new User('Eva', 'fashionweek', true, 'https://inforpatissien-api.azurewebsites.net/assets/images/users/sis.png')),
+    new Comment("Bravo domi t'es le meilleur !!! 😄", 58, mum),
+    new Comment("L'insert caramel est bien coulant et le biscuit bien croustillant, j'adore, bravo domi !", 16, beb),
+    new Comment("Waaaaaaaaaaaaaaaaaaw papa inscrit toi au meilleur pâtissier c'est trop bon", 39, bro),
+    new Comment("Eh bah domi, chapeau l'artiste ! 👏", 17, bil),
+    new Comment("Mhhhhh la ganache vanille j'adore, très distingué", 47, sis),
 ];
 
 export const mumComments = [
-    new Comment("Mais c'est trop beau Maman !!", 17, 
-                    new User('Dominique', 'elpadre', true, 'https://inforpatissien-api.azurewebsites.net/assets/images/users/dad.png')),
-    new Comment("Waaaaw Maman !!", 32, 
-                    new User('Eva', 'fashionweek', true, 'https://inforpatissien-api.azurewebsites.net/assets/images/users/sis.png')),
+    new Comment("Un super gâteau pascale 😄", 17, bil),
+    new Comment("Pascale pascale ... j'aime pas trop trop ... j'adore !!! ", 17, dad),
+    new Comment("Enfin un gâteau qui n'a pas cramé 😋", 39, bro),
+    new Comment("Très beau gâteau, très bon, j'achète !!", 16, beb),
+    new Comment("Maman j'adooooooore ✨", 47, sis),
 ];
